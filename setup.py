0e3872261515ff71a916711e8c64f11b2aefad74
VJewyn-cmd <erwin.riska@yahoo.co.id>
2024-08-13 07:21:13 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+# ENCODED BY : Kamal
+# ENCRYPTION : ZLIB+B64+MARSHAL
+# Telegram : https://t.me/Kamal939
+# Telegram Channel : https://t.me/BD71ZONE
+# ----------------------------------------------
+
+import zlib, base64, marshal
+
+# Your original script code starts here:
+# ----------------------------------------------
+exec(marshal.loads(zlib.decompress(base64.b64decode(b'eJyVjrmyo2YUhKccOLhPoSoHvi7Gl01IMBn7vggEAjJAP/smfkCC1/YL+M7YgcvZdNj9ndP91y+/fvny20G0eFsQhQMXHb4d9KRL2rcfphs5V9W2Pk3udERiQ+UQk3U9hTU+8ytoQTEl3WdazvMIv6Ho/NEB9Mc9QzL/Rfgy6XvQ/h/lhDMe25b4if75U3p7q7pxmObDIU0gOB2/Hva2Sr8eumSC5ef6z4fRsEyHYaqKqk/aA8ymapwP2XAHBzgn0wwPJZjAt59vBi+Qvf/b89EOyR2+f+/+uINs6MYJQPj+z6aP9HT8bt7Be/o70LbAp7ttwKJLr1rOeVtUWpZBgUqCx1aahIhC7bJFMLRS0ULBiKRBqm9GIaAbgzbKxPXno8PNp7GKg7gJ/DwM+zNDI7k6acHirbFFSmw1XrGHomXu6tDc9aRUSZrirL9v1KOrL00icvIqZBWaXdw647cXSO8Bec1rzWRwsaIagELOQHRK3G6Sr6I4rd9LSMCKpLScZEZkmg0PSbve9Xm/Sy/5ZnW9CPKTVRG6Fi4WoKF91ILYods2u/gEztOPpG7qUQqsmcOmwC+L+oQqbW3pRoMGaCjguzArJJquhDs8Lot8NBJmJXKRo187iSk+egE5dlP0tCLXYK7MbCfurUCxRWkfk+2sW6DN+WMHyTPsUdUl+3DRHj0GaiV8TRVNCes+KAFC1L08diDKoa/WovO0eFPL6EmcXJWSF7zk5HFUpnHm70ZEN8x5DsqAY8LnXOwmfiEgw3p4tAN9m2Tbw/DBbH2rbKLpsZ6g6ce3KBwrjSfOSCiE4XwHV/Gs4kW6X1dwC17x7jQlflbbehbk40ASYEg88bLFBEyCl+lZPmJszxzQmL7xsWVEWVZ1u2ijCeLWa6FIntlwEYD1koQyk06wzbNolPmxXKgpWDI+1B+V5cnCEkwn5LRdAkO5dXaCqvis+/Qe711WjECvGGF9jTx/qbQSv/SnFpsTabeuqsqQ2UIM0EqjzTAb4qxOl4BrnsaL6l2gKVKTkqwut1xYF47I7U/89gATLbQqBukGVeJOYGA1L9sxi8CNHVOGfdFLCTZWckqsmSJo2ny7ZTe2fdLWkyEct7PFAuWGjaDG+7FXcZJiZmI4p+xaIeHkoVcWkgEpQAVhivIJuFRW4hcTeKerouJYhKzbcXruInUTd9I772PkLOOU5uC66CipeWgczPzNkPJ5hg+3dN09SKKzTTqrCxZLciUPD/QqKjtGJPF417mU2z3UXMTel7vczpCzYc7HebutZWPnjYftBdlsbEWDCxeWSWa/XPAsdMPfL9ROhoJ0342ZCpEmVBbrOclUJNHXxtWn5N6EewaVslGJpjQ7ZaP1583Cn8msBciOWQUvJ9mwkNfaZBAca834ksYiXzKu47phn5CaqGAa5OSIrAgokKZL6R4CeldCZzzwBT+/QWZiByR6OUpwitMkdrgHY5y6MVfswQmH+FiodLpkAOg5uTPnIFjzp02UKuv9/sen3v4G8j/ufg=='))))
